d7ee5d502d6e0c3d823aadab0992a17130cac6ec
RESTY NASIMBWA <54865360+restynan@users.noreply.github.com>
2020-03-12 16:08:15 -0500

    eureka

---
+++ eurekaDemo/src/main/java/com/miu/eurekaDemo/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.miu.eurekaDemo;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(EurekaDemoApplication.class);
+	}
+
+}
---
+++ eurekaDemo/src/test/java/com/miu/eurekaDemo/EurekaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miu.eurekaDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
