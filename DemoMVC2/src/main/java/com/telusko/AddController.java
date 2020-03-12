79b4e0e7df30935de22c9677e79e9dcc7661a969
Resty Nasimbwa <restynan@gmail.com>
2020-03-11 23:01:41 -0500

    springMVCAnotation --XML deleted

--- DemoMVC2/src/main/java/com/telusko/AddController.java
+++ DemoMVC2/src/main/java/com/telusko/AddController.java
@@ -7,18 +7,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.telusko.service.AddService;
+
+
 @Controller
-public class addController {
+public class AddController {
 	@RequestMapping("/add")
-	public ModelAndView add(HttpServletRequest request ,HttpServletResponse response) {
-		
-		
-		int i=Integer.parseInt(request.getParameter("t1"));
-		int j=Integer.parseInt(request.getParameter("t2"));
-		int k=i+j;
-	ModelAndView mv= new ModelAndView();
-	mv.setViewName("display.jsp");
-	mv.addObject("result",k);
+	public ModelAndView add(HttpServletRequest request, HttpServletResponse response) {
+
+		int i = Integer.parseInt(request.getParameter("t1"));
+		int j = Integer.parseInt(request.getParameter("t2"));
+		AddService as = new AddService();
+		int k = as.addMethod(i,j);
+	
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("display.jsp");
+		mv.addObject("result", k);
 		return mv;
 	}
 
